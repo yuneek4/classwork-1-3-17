45ad07744f117c7aba391e9d9c1d7a62815cc91c
yuneek4 <yuneek4@gmail.com>
2017-01-03 14:48:54 -0500

    This is the classwork about calculators and how to do them

---
+++ classwork 1:3py.py
@@ -0,0 +1,19 @@
+# Calculator 1
+'''
+Fahrenheit= input('put in the degree fahrenheit')
+F= float(Fahrenheit)
+
+Celcius= F-32* 1.8
+print('The temperture in celcius', Celcius)
+'''
+# Calculator 2
+
+a= input('put in the top base measure')
+a= float(a) 
+b= input('put in the total botton base measure')
+b= float(b)
+h= input('put in the measure of the height of the triangle inside the trapezoid')
+h= float(h)
+
+Area= ((a+b)/2)*h
+print('The total area of the trapezoid', Area) 
